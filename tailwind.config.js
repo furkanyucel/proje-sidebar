a8341fa24d46c39d8f1799e49689f8cf28854083
Furkan Yücel <47148709+furkanyucel@users.noreply.github.com>
2022-11-13 06:37:58 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: "#5460E6",
+      secondary: "#000000",
+      white: "#ffffff",
+    },
+  },
+  plugins: [],
+}
